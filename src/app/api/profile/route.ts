ac4844c14f489b4ba13e9042d85c0e4152f9250f
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-02-05 10:32:46 +0700

    wip: profile user

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -3,53 +3,19 @@ import { NextResponse } from "next/server"
 
 const prisma = new PrismaClient()
 
-export async function POST(req: Request) {
-  if (req.method !== "POST") {
-    return NextResponse.json({ message: "Method Not Allowed" })
-  }
-
-  const data = await req.json()
-
-  try {
-    console.log("Received data:", data)
-
-    if (!data.name) {
-      return NextResponse.json({
-        message: "Invalid input. category name are required.",
-      })
-    }
-
-    const newCategory = await prisma.categories.create({
-      data: {
-        name: data.name,
-      },
-    })
-
-    return NextResponse.json({
-      message: "Category created successfully",
-      Category: newCategory,
-    })
-  } catch (error) {
-    console.error("Error processing request:", error)
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Something Went Wrong",
-      },
-      {
-        status: 500,
-      }
-    )
-  }
-}
-
 export async function GET(req: Request) {
+  const data = {
+    id:1
+  }
+
   if (req.method === "GET") {
     try {
-      const categories = await prisma.categories.findMany()
-      return NextResponse.json({ categories })
+      const users = await prisma.users.findFirst({
+        where: { id: data.id },
+      })
+      return NextResponse.json({ users })
     } catch (error) {
-      console.error("Error fetching categories:", error)
+      console.error("Error fetching user:", error)
       return NextResponse.json(
         {
           success: false,
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,13 +10,47 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons"
 import { useRouter } from "next/navigation"
 import Image from "next/image"
 import Profile from "../images/blank_profile.jpeg"
+import { useEffect, useState } from "react"
 
 // export const metadata = {
 //   title: 'Contact | Dataxperience'
 // }
 
-export default function ContactUs() {
+interface Profile {
+    id: number
+    name: string
+    email: string
+    country: string
+    password: string
+}  
 
+export default function ContactUs() {
+    const [profile, setProfile] = useState<Profile[]>([])
+    const [error, setError] = useState('')
+    const fetchProfile = async () => {
+        try {
+          const response = await fetch('/api/profile', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+          console.log(response)
+          if (response.ok) {
+            const { users } = await response.json()
+            setProfile(users)
+          } else {
+            const error = await response.json()
+            setError(error.message)
+          }
+        } catch (error) {
+          console.error('Error fetching news:', error)
+          setError('Internal Server Error')
+        }
+      }
+      useEffect(() => {
+        fetchProfile()
+      }, [])
     return (
         <div>
             <Navbar />
@@ -42,11 +76,11 @@ export default function ContactUs() {
                     <form className="mx-auto">
                         <div className="mb-5">
                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter your password" required autoComplete="off"/>
+                            <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter your password" required autoComplete="off" defaultValue={profile.name}/>
                         </div>
                         <div className="mb-5">
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter your email" required autoComplete="off"/>
+                            <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="Enter your email" required autoComplete="off" defaultValue={profile.email}/>
                         </div>
                         <div className="mb-5">
                             <label htmlFor="country" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country </label>
@@ -56,7 +90,7 @@ export default function ContactUs() {
                         </div>
                         <div className="mb-5">
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Change Password</label>
-                            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required autoComplete="off"/>
+                            <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" required autoComplete="off" defaultValue={profile.password}/>
                         </div>
                         <div className="w-full flex justify-end">
                             <button type="submit" className="text-white bg-[#C22020] hover:bg-[#a01b1b] font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Update Profile</button>
