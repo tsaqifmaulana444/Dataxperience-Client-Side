a8ecdeb3a90ecddaf9281e70849d59d958d76f95
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-02-05 11:00:29 +0700

    feat: profile user

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -1,21 +1,36 @@
 import { PrismaClient } from "@prisma/client"
 import { NextResponse } from "next/server"
+import bcrypt from 'bcryptjs'
+const saltRounds = 10
 
 const prisma = new PrismaClient()
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  const id = parseInt(params.id, 10)
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+  const id = parseInt(params.id)
 
-  await prisma.categories.delete({
+  const  data  = await request.json()
+  
+  const hashedPassword = await bcrypt.hash(data.password, saltRounds)
+
+  const users = await prisma.users.update({
     where: {
       id: id,
     },
+    data: {
+      name: data.name,
+      email: data.email,
+      country: data.country,
+      profile: data.profile,
+      password: hashedPassword,
+      updated_at: new Date(),
+    },
   })
 
+  //return response JSON
   return NextResponse.json(
     {
       success: true,
-      message: "Category Deleted!",
+      message: "Data Updated!",
     },
     {
       status: 200,
